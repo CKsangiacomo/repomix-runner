a3d233acd0ae9e18aa74518e37a57cadb6f2cc88
P Consavari <pconsavari@gmail.com>
2025-09-17 13:32:28 -0700

    repomix: update artifact (2025-09-17T20:32:28Z)

---
+++ status.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/json');
+$f=__DIR__.'/CKgit/CKgitstatus.xml';
+echo json_encode([
+  'ok'=>file_exists($f),
+  'url'=>'https://repomix.consavari.com/CKgit/CKgitstatus.xml',
+  'size_bytes'=>file_exists($f)?filesize($f):null,
+  'updated_at'=>file_exists($f)?gmdate('c', filemtime($f)):null,
+]);
