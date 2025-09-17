6266512a33cb929bef6ac0da418c6c297bc5ebc2
P Consavari <pconsavari@gmail.com>
2025-09-17 14:38:40 -0700

    repomix: deploy-now 2025-09-17T21:38:40Z

--- status.php
+++ status.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: application/json');
-$f=__DIR__.'/CKgit/CKgitstatus.xml';
+$f=__DIR__.'/CKgit/CKgitstatus.txt';
 echo json_encode([
   'ok'=>file_exists($f),
-  'url'=>'https://repomix.consavari.com/CKgit/CKgitstatus.xml',
+  'url'=>'https://repomix.consavari.com/CKgit/CKgitstatus.txt',
   'size_bytes'=>file_exists($f)?filesize($f):null,
   'updated_at'=>file_exists($f)?gmdate('c', filemtime($f)):null,
 ]);
